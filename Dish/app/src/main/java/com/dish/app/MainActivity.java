0c5ae20c8a6622b800fd736d289429db432ca323
kabulqz <123zelek@gmail.com>
2025-04-21 22:15:06 +0200

    changed nav buttons
    now they change colors !!!!!!!11!!11!111!!1!111!!!!!!

--- Dish/app/src/main/java/com/dish/app/MainActivity.java
+++ Dish/app/src/main/java/com/dish/app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.dish.app;
 
+import android.annotation.SuppressLint;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private View homeButton, searchButton, addNewRecipeButton, profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,33 +27,26 @@ public class MainActivity extends AppCompatActivity {
         View addNewRecipeButton = findViewById(R.id.addNewRecipeButton);
         View profileButton = findViewById(R.id.profileButton);
 
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Home Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+        View[] buttons;
+        buttons = new View[]{homeButton, searchButton, addNewRecipeButton, profileButton};
+        for(View button : buttons) {
+            button.setOnClickListener(view ->{
+                for(View other : buttons) {
+                    int color = (other == view) ? Color.parseColor("#000000") : Color.parseColor("#343A40");
+                    other.setBackgroundTintList(ColorStateList.valueOf(color));
+                }
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Search Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
+                if(view.getId() == R.id.homeButton) {
+                    Toast.makeText(this, "Home Button Clicked", Toast.LENGTH_SHORT).show();
+                } else if(view.getId() == R.id.searchButton) {
+                    Toast.makeText(this, "Search Button Clicked", Toast.LENGTH_SHORT).show();
+                } else if(view.getId() == R.id.addNewRecipeButton) {
+                    Toast.makeText(this, "Add New Recipe Button Clicked", Toast.LENGTH_SHORT).show();
+                } else if(view.getId() == R.id.profileButton) {
+                    Toast.makeText(this, "Profile Button Clicked", Toast.LENGTH_SHORT).show();
+                }
 
-        addNewRecipeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Add New Recipe Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        profileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Profile Clicked", Toast.LENGTH_SHORT).show();
-            }
-        });
-
+            });
+        }
     }
 }
