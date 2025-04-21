f8eca659bd3b44b8c9ff11ad22c527d79babf57f
kabulqz <123zelek@gmail.com>
2025-04-21 13:03:07 +0200

    Navigational buttons
    added buttons that only display text {for now} when you click on them

--- Dish/app/src/main/java/com/dish/app/MainActivity.java
+++ Dish/app/src/main/java/com/dish/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.dish.app;
 
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,5 +16,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        View homeButton = findViewById(R.id.homeButton);
+        View searchButton = findViewById(R.id.searchButton);
+        View addNewRecipeButton = findViewById(R.id.addNewRecipeButton);
+        View profileButton = findViewById(R.id.profileButton);
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Home Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Search Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        addNewRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Add New Recipe Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Profile Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
